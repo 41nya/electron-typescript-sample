42280829537626f79d6fec404b962ad72ec06250
sny41 <sny.cho@gmail.com>
2020-10-04 17:52:16 +0900

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: __dirname +  "/src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        exclude: /node_modules/,
+        loader: "ts-loader"
+      }
+    ]
+  },
+  target: 'electron-main',
+  devServer: {
+    port: 8080,
+    open: "Google Chrome",
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
